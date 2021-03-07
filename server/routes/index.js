5cf0bff1914af130d11125924c502e4fe3c39b76
Simon <gtrsimon@gmail.com>
2021-03-07 18:00:42 -0500

    Init ES6 Express project

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+// var express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+export default router;
